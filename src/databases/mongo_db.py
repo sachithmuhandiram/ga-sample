b77cf63fee1738a6fd842fac240998483057b583
sachithmuhandiram <sachithnalaka@gmail.com>
2024-05-12 22:12:23 +0530

    Fixed Mongo db record fetching

--- src/databases/mongo_db.py
+++ src/databases/mongo_db.py
@@ -1,7 +1,21 @@
+import configparser
+from pymongo import MongoClient
+
+
 class MongoDBConnection:
 
-    def initialize_connection(self):
+    def __init__(self) -> None:
+        # config = configparser.ConfigParser()
+
+        # config.read("config/config.ini")
+        self.connection_url = "mongodb://localhost:27017"  # config.get("Mongo", "url")
+        self.database = "sample_db"  # config.get("Mongo", "database")
+
+    def get_mongodb_connection(self):
+        mongo_db_conn = MongoClient(self.connection_url)
+        mongo_db = mongo_db_conn[self.database]
         print("Connecting to MongoDB")
+        return mongo_db
 
     def insert_records(self, number_of_records):
         if number_of_records == "single":
