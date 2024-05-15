c52d83691160706918cddd9a1287582e10925704
sachithmuhandiram <sachithnalaka@gmail.com>
2024-05-15 21:49:32 +0530

    Added JS files to the project

--- src/services/services/urls.py
+++ src/services/services/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("user_module/", include("user_module.urls")),
     path("ga_module/", include("ga_module.urls")),
     path("course_module/", include("course_module.urls")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- src/services/user_module/views.py
+++ src/services/user_module/views.py
@@ -5,11 +5,13 @@ from databases.database_connection import DatabaseConnection
 
 # Create your views here.
 def index(request):
-    mongo_db_connection = DatabaseConnection.create_database_connection("MongoDB")
+    # mongo_db_connection = DatabaseConnection.create_database_connection("MongoDB")
 
-    mongo_db = mongo_db_connection.connect_to_mongo_database()
+    # mongo_db = mongo_db_connection.connect_to_mongo_database()
 
-    mongo_db_collection = mongo_db["sample_course"]
+    # mongo_db_collection = mongo_db["sample_course"]
 
-    otem = list(mongo_db_collection.find({"course_code": "ECX6236"}))
-    return HttpResponse(otem)
+    # otem = list(mongo_db_collection.find({"course_code": "ECX6236"}))
+    # return HttpResponse(otem)
+
+    return render(request, "user_module/login.html")
