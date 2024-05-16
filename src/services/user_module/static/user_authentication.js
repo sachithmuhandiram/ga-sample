5c3e69ac4ee2246010cee93256d9b3ed7bebe5c7
sachithmuhandiram <sachithnalaka@gmail.com>
2024-05-16 19:54:14 +0530

    Fixed JS loading issue after bringing JS script tag at the bottom of the html file.

---
+++ src/services/user_module/static/user_authentication.js
@@ -0,0 +1,14 @@
+function get_user_password() {
+
+    
+}
+
+// Here we can not go for => function, as this has limited area of accesss
+document.getElementById("submit_btn").addEventListener("click", function () {
+    alert("Hello");
+    // this.event.preventDefault();  
+
+    // let userName = this.id("exampleInputName");
+    // console.log(userName);
+
+});
