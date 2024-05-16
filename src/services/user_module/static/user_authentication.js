ecc68f286be065c3b7ad511e73bb8c4bf7e58cc4
sachithmuhandiram <sachithnalaka@gmail.com>
2024-05-16 21:46:42 +0530

    Connecting to MongoDB using JS side is not working. Its having client-side isses

--- src/services/user_module/static/user_authentication.js
+++ src/services/user_module/static/user_authentication.js
@@ -1,6 +1,25 @@
-function get_user_password() {
+import connectToMongoDB from './mongo_db.js';
 
-    
+async function searchForARecord(user_name) {
+    try {
+        // Connect to the database
+        const db = await connectToMongoDB();
+
+        // Perform database operations
+        const collection = db.collection('sample_course');
+        const result = await collection.findOne({ keyName: user_name });
+
+        if (result) {
+            console.log('Found document:', result);
+        } else {
+            console.log('No document found');
+        }
+
+        // Close the connection
+        db.client.close();
+    } catch (error) {
+        console.error('Error occurred while performing database operation:', error);
+    }
 }
 
 // Here we can not go for => function, as this has limited area of accesss
@@ -10,6 +29,9 @@ document.getElementById("submit_btn").addEventListener("click", function (e) {
     e.preventDefault();  
 
    let userName = document.getElementById("exampleInputName").value;
-    console.log(userName);
+
+    let password = searchForARecord(user_name);
+    console.log(password);
 
 });
+
