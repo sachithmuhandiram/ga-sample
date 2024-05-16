752f5917d3f9a8d8892d014d2c2c049755a24d09
sachithmuhandiram <sachithnalaka@gmail.com>
2024-05-16 20:25:19 +0530

    Fixed JS data retrival from form element

--- src/services/user_module/static/user_authentication.js
+++ src/services/user_module/static/user_authentication.js
@@ -4,11 +4,12 @@ function get_user_password() {
 }
 
 // Here we can not go for => function, as this has limited area of accesss
-document.getElementById("submit_btn").addEventListener("click", function () {
-    alert("Hello");
-    // this.event.preventDefault();  
+document.getElementById("submit_btn").addEventListener("click", function (e) {
 
-    // let userName = this.id("exampleInputName");
-    // console.log(userName);
+    //alert("Hello");
+    e.preventDefault();  
+
+   let userName = document.getElementById("exampleInputName").value;
+    console.log(userName);
 
 });
