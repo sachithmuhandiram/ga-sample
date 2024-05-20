f414a88425b78ddb387eb375dbcab5c8a1e55a4e
sachithmuhandiram <sachithnalaka@gmail.com>
2024-05-20 22:14:23 +0530

    Activities can be used to the course listed as a check boxes, initial steps are created

--- src/services/course_module/views.py
+++ src/services/course_module/views.py
@@ -8,6 +8,7 @@ import json
 mongo_db_connection = DatabaseConnection.create_database_connection("MongoDB")
 
 mongo_db = mongo_db_connection.connect_to_mongo_database()
+mongo_course_activity_collection = mongo_db["course_activities"]
 
 
 class CreateNewActivity(forms.Form):
@@ -20,12 +21,19 @@ def create_an_activity(request):
 
 
 def define_a_course(request):
-    return render(request, "course_module/define_a_course.html")
+    # Get data from mongo db about the all course activities
+    course_activities = mongo_course_activity_collection.find({})
+
+    return render(
+        request,
+        "course_module/define_a_course.html",
+        {"course_activities": course_activities},
+    )
 
 
 def save_an_activity(request):
     if request.method == "POST":
-        mongo_db_collection = mongo_db["course_activities"]
+
         # Get the new activity from the form
         new_activity = request.POST.get("new_activity")
         has_groups = request.POST.get("has_groups")
@@ -38,7 +46,7 @@ def save_an_activity(request):
         # Create a python dictionary named course_data
         activity_data = {"activity_name": new_activity, "has_groups": has_groups}
 
-        mongo_db_collection.insert_one(activity_data)
+        mongo_course_activity_collection.insert_one(activity_data)
 
         return render(request, "course_module/create_an_activity.html")
     else:
