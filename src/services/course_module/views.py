c8cf031013fdf535542be68343b5159ae6f08170
sachithmuhandiram <sachithnalaka@gmail.com>
2024-05-20 21:09:43 +0530

    Fixed: data inserting to mongodb for activities fixed, but there is an issue with activity_name field data

--- src/services/course_module/views.py
+++ src/services/course_module/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from django.shortcuts import render
 from .models import CourseActivity
 from django import forms
+from databases.database_connection import DatabaseConnection
+import json
+
+mongo_db_connection = DatabaseConnection.create_database_connection("MongoDB")
+
+mongo_db = mongo_db_connection.connect_to_mongo_database()
 
 
 class CreateNewActivity(forms.Form):
@@ -15,14 +21,15 @@ def create_an_activity(request):
 
 def save_an_activity(request):
     if request.method == "POST":
-
+        mongo_db_collection = mongo_db["course_activities"]
         # Get the new activity from the form
-        new_activity = request.POST.get("new_activity")
+        new_activity = request.POST.get("activity")
         has_groups = request.POST.get("has_groups")
 
-        # Save the new activity to the database
-        activity = CourseActivity(activity_name=new_activity, has_groups=has_groups)
-        # should be with pymongo
+        # Create a python dictionary named course_data
+        activity_data = {"activity_name": new_activity, "has_groups": has_groups}
+
+        mongo_db_collection.insert_one(activity_data)
 
         return render(request, "course_module/create_an_activity.html")
     else:
