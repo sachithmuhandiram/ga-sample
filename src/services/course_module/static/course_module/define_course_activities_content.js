bda63218b0f8249b8d49ad775a03de3691c04e5a
sachithmuhandiram <sachithnalaka@gmail.com>
2024-06-01 11:55:25 +0530

    Text input not functioning

--- src/services/course_module/static/course_module/define_course_activities_content.js
+++ src/services/course_module/static/course_module/define_course_activities_content.js
@@ -82,7 +82,7 @@ function createActivityElement(activity) {
         var input = document.createElement('input');
         input.type = "text";
         input.name = activity[activity_name];
-        input.classList.add("form-control","inline-input");
+        input.classList.add("form-control","inline-input","col-md-6");
         input.value = "";
         formDiv.appendChild(input);
 
