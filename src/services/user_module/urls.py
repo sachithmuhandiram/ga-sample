c2592d6f6030b2e7c0a5c9b23e007457dd73344f
sachithmuhandiram <sachithnalaka@gmail.com>
2024-06-09 22:08:06 +0530

    feat: Update login page with Bootstrap styling and form validation

--- src/services/user_module/urls.py
+++ src/services/user_module/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 app_name = "user_module"
 
-urlpatterns = []
+urlpatterns = [
+    path("", views.login, name="login"),
+    path("authenticate_user/", views.authenticate_user, name="authenticate_user"),
+    path("authorize_user/", views.authorize_user, name="authorize_user"),
+    path("register", views.register, name="register"),
+    path("register_user", views.register_user, name="register_user"),
+]
--- src/services/user_module/views.py
+++ src/services/user_module/views.py
@@ -1,1 +1,70 @@
-#
+from django.shortcuts import render
+from django.shortcuts import render
+from .models import User
+from django import forms
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+import json
+from bson import json_util
+
+
+# View for login page
+def login(request):
+    return render(request, "user_module/login.html")
+
+
+# view for user authentication
+@api_view(["POST"])
+def authenticate_user(request):
+    if request.method == "POST":
+        username = request.data["username"]
+        password = request.data["password"]
+        user = User.objects.get(username=username)
+        if user.authenticate_user(username, password):
+            return Response({"message": "User authenticated"})
+        else:
+            return Response({"message": "User not authenticated"})
+
+
+# view for user authorization
+@api_view(["POST"])
+def authorize_user(request):
+    if request.method == "POST":
+        username = request.data["username"]
+        role = request.data["role"]
+        user = User.objects.get(username=username)
+        if user.authorize_user(role):
+            return Response({"message": "User authorized"})
+        else:
+            return Response({"message": "User not authorized"})
+
+
+# view for user registration page
+def register(request):
+    return render(request, "user_module/register.html")
+
+
+# view for user registration
+@api_view(["POST"])
+def register_user(request):
+    if request.method == "POST":
+        username = request.data["username"]
+        password = request.data["password"]
+        email = request.data["email"]
+        first_name = request.data["first_name"]
+        last_name = request.data["last_name"]
+        mobile_number = request.data["mobile_number"]
+        role = request.data["role"]
+        course = request.data["course"]
+        user = User()
+        user.add_new_user(
+            username,
+            password,
+            email,
+            first_name,
+            last_name,
+            mobile_number,
+            role,
+            course,
+        )
+        return Response({"message": "User registered"})
